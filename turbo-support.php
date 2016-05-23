9a87044872b3d4dc1c255ab440f900b03ca41b8b
Brian Scaturro <scaturrob@gmail.com>
2016-05-23 15:22:22 -0400

    Adds sass build process

---
+++ turbo-support.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package Turbo Support
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Turbo Support
+Plugin URI: http://prophoto.com
+Description: Turbo mode for the ProPhoto support site
+Author: Brian Scaturro
+Version: 1.0.0
+Author URI: http://github.com/brianium
+*/
+
+// require hooks
+require 'hooks/index.php';
