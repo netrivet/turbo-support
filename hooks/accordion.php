23e93fd0176103d5a545ac0fde48d2700e3fa6f6
Steve Post <postmastersteve@gmail.com>
2016-10-04 16:08:10 -0400

    Add accordion attributes for autoclose and clicktoclose to default on

--- hooks/accordion.php
+++ hooks/accordion.php
@@ -5,10 +5,15 @@
  * behind our fixed header. See: https://wordpress.org/plugins/accordion-shortcodes/other_notes/
  */
 add_filter('shortcode_atts_accordion', function($atts) {
-    if (isset($atts['scroll']) && $atts['scroll']) {
+    if (isset($atts['scroll']) || $atts['scroll']) {
         $atts['scroll'] = 80;
-        $atts['openfirst'] = true;
+    }
+    if (!isset($atts['autoclose'])) {
+        $atts['autoclose'] = true;
+    }
+    if (!isset($atts['clicktoclose'])) {
         $atts['clicktoclose'] = true;
     }
+
     return $atts;
 }, 10, 3);
