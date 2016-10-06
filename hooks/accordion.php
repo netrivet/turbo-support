61dc72df55439c6464ac1bb992542ea603c3b8bc
Steve Post <postmastersteve@gmail.com>
2016-10-06 12:25:08 -0400

    clarify conditional logic and add comments

--- hooks/accordion.php
+++ hooks/accordion.php
@@ -2,17 +2,22 @@
 
 /**
  * Sets offset for scrolling accordion items to ensure they don't fall
- * behind our fixed header. See: https://wordpress.org/plugins/accordion-shortcodes/other_notes/
+ * behind our fixed header, and default-on 'autoclose' and 'clicktoclose'
+ * See: https://wordpress.org/plugins/accordion-shortcodes/other_notes/
  */
 add_filter('shortcode_atts_accordion', function($atts) {
-    if (isset($atts['scroll']) = $atts['scroll']) {
-        $atts['scroll'] = 80;
+    
+    if ( !isset( $atts['scroll'] || 						// shortcode doesn't have 'scroll' or ...
+    	 ( isset( $atts['scroll'] ) && $atts['scroll'] ) {	// shortcode is set 'scroll=true' ...
+    	$atts['scroll'] = 80;								// turn scroll ON with 80px of top offset
+    }														// setting 'scroll=false' will disable scroll
+    
+    if ( !isset( $atts['autoclose'] ) ) {					// shortcode doesn't have 'autoclose' ...
+        $atts['autoclose'] = true;							// turn autoclose ON
     }
-    if (!isset($atts['autoclose'])) {
-        $atts['autoclose'] = true;
-    }
-    if (!isset($atts['clicktoclose'])) {
-        $atts['clicktoclose'] = true;
+
+    if ( !isset( $atts['clicktoclose'] ) ) {				// shortcode doesn't have 'clicktoclose' ...
+        $atts['clicktoclose'] = true;						// turn clicktoclose ON
     }
 
     return $atts;
