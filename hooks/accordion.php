e04f22a71a9f9fb911e9ac21bf3156b6e49833ca
Steve Post <postmastersteve@gmail.com>
2016-10-06 12:45:09 -0400

    created new filter per plugin documentation to set defaults

--- hooks/accordion.php
+++ hooks/accordion.php
@@ -1,6 +1,20 @@
 <?php
 
 /**
+ * Changes default behavior of accordion shortcode parameters.
+ * See: https://wordpress.org/plugins/accordion-shortcodes/other_notes/
+ */
+add_filter('shortcode_atts_accordion', 'set_accordion_shortcode_defaults', 10, 3);
+function set_accordion_shortcode_defaults($atts) {
+
+    // Override the autoclose and clicktoclose settings
+    $atts['autoclose'] = true;
+    $atts['clicktoclose'] = true;
+    
+    return $atts;
+}
+
+/**
  * Sets offset for scrolling accordion items to ensure they don't fall
  * behind our fixed header, and default-on 'autoclose' and 'clicktoclose'
  * See: https://wordpress.org/plugins/accordion-shortcodes/other_notes/
@@ -11,14 +25,7 @@ add_filter('shortcode_atts_accordion', function($atts) {
     	 ( isset( $atts['scroll'] ) && $atts['scroll'] ) ) {	// shortcode is set 'scroll=true' ...
     	$atts['scroll'] = 80;									// turn scroll ON with 80px of top offset
     }															// setting 'scroll=false' will disable scroll
-    
-    if ( !isset( $atts['autoclose'] ) ) {						// shortcode doesn't have 'autoclose' ...
-        $atts['autoclose'] = true;								// turn autoclose ON
-    }
-
-    if ( !isset( $atts['clicktoclose'] ) ) {					// shortcode doesn't have 'clicktoclose' ...
-        $atts['clicktoclose'] = true;							// turn clicktoclose ON
-    }
 
     return $atts;
 }, 10, 3);
+
