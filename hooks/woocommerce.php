761543c05854a905713b19dcafe6d8e8cfa79ed1
Matt Dietsche <matt@netrivet.com>
2019-08-27 14:09:07 -0400

    revisions

--- hooks/woocommerce.php
+++ hooks/woocommerce.php
@@ -4,7 +4,6 @@ add_action( 'init', 'format_single_product_page', 100);
 
 function format_single_product_page() {
     remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
-    remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_images', 20 );
     add_action( 'woocommerce_before_single_product_summary', function() {
         the_content();
     }, 10 );
