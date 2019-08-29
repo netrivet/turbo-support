0f1cfe94fc9cab9e9ab72cea278f02306b89e04c
meatwad5675 <matt@netrivet.com>
2019-08-29 13:34:12 -0400

    woocommerce tweaks based on support team feedback (#11)

--- hooks/woocommerce.php
+++ hooks/woocommerce.php
@@ -4,7 +4,6 @@ add_action( 'init', 'format_single_product_page', 100);
 
 function format_single_product_page() {
     remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
-    remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_images', 20 );
     add_action( 'woocommerce_before_single_product_summary', function() {
         the_content();
     }, 10 );
@@ -21,3 +20,14 @@ function pp_woo_cart() {
 }
 
 add_shortcode('pp_woo_cart', 'pp_woo_cart');
+
+function set_order_notes_label($args, $key) {
+    if ($key === 'order_comments') {
+        $args['label'] = 'Specify a Support Tech you have been working with';
+        $args['placeholder'] = 'Dan, Benjamin, or Steve';
+    }
+
+    return $args;
+}
+
+add_filter('woocommerce_form_field_args', 'set_order_notes_label', 10, 2);
