480f4e41797433103343586107d7b523116a19bb
Steve Post <postmastersteve@gmail.com>
2019-09-10 16:20:20 -0400

    removing wpsync hooks because we use WP Migrate DB Pro plugin for sync now

--- hooks/index.php
+++ hooks/index.php
@@ -4,7 +4,6 @@ require_once __DIR__ . '/breadcrumbs.php';
 require_once __DIR__ . '/accordion.php';
 require_once __DIR__ . '/search.php';
 require_once __DIR__ . '/pages.php';
-require_once __DIR__ . '/wpsync.php';
 add_action('plugins_loaded', function () {
     if (class_exists('\WooCommerce')) {
         require_once __DIR__ . '/woocommerce.php';
